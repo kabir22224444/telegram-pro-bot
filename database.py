10e1477e5743ed767dc2b297bf26a6e068c5467a
kabir22224444 <ranjeetchoudhary0094@gmail.com>
2026-02-18 08:21:29 +0530

    Implement database connection and order management
    Added functions to initialize the database and manage orders.

--- database.py
+++ database.py
@@ -1,1 +1,39 @@
 add database file
+import psycopg2, os
+from datetime import datetime
+
+DB_URL = os.getenv("DB_URL")
+
+def get_conn():
+    return psycopg2.connect(DB_URL)
+
+def init_db():
+    with get_conn() as conn:
+        cur = conn.cursor()
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS orders(
+            id SERIAL PRIMARY KEY,
+            order_id TEXT,
+            user_id BIGINT,
+            service TEXT,
+            game TEXT,
+            status TEXT,
+            created_at TIMESTAMP
+        )
+        """)
+        conn.commit()
+
+def save_order(order_id, user_id, service, game, status="PENDING"):
+    with get_conn() as conn:
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO orders(order_id,user_id,service,game,status,created_at) VALUES(%s,%s,%s,%s,%s,%s)",
+            (order_id,user_id,service,game,status,datetime.now())
+        )
+        conn.commit()
+
+def update_status(order_id, status):
+    with get_conn() as conn:
+        cur = conn.cursor()
+        cur.execute("UPDATE orders SET status=%s WHERE order_id=%s",(status,order_id))
+        conn.commit()
